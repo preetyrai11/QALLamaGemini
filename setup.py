29a57d1f48d11c831fc22574f760584de9d2c0df
preetyrai11 <drpreetyrai@gmail.com>
2024-08-02 12:07:43 +0530

    code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'preetyrai11',
+    author_email= 'drpreetyrai@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
